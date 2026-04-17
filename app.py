89462ad6f613650dc5b734e6f8d8d96aad2966cf
Ivan Quirante <ivangonquir@gmail.com>
2026-04-17 13:23:00 +0200

    Flask Producción

--- app.py
+++ app.py
@@ -75,6 +75,7 @@ def api_reservas():
 
 if __name__ == "__main__":
     try:
-        app.run(host="0.0.0.0", port=5000, debug=False)
+        from waitress import serve
+        serve(app, host="0.0.0.0", port=5000)
     finally:
         scheduler.shutdown()
